0e8fd64df05cf70cfe572125d3e01995d8ab0884
Ihsan28 <ihsanmohammadalaqib@gmail.com>
2023-02-24 03:35:44 +0600

    match fragments reduced

--- app/src/main/java/com/ihsan/cricplanet/adapter/viewpager/TabMatchAdapter.kt
+++ app/src/main/java/com/ihsan/cricplanet/adapter/viewpager/TabMatchAdapter.kt
@@ -1,28 +1,78 @@
 package com.ihsan.cricplanet.adapter.viewpager
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ihsan.cricplanet.model.Tab
-import com.ihsan.cricplanet.ui.fragment.match.*
+import com.ihsan.cricplanet.ui.fragment.match.MatchesFragment
 
-class TabMatchAdapter (manager: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter(manager,lifecycle) {
-    companion object{
+class TabMatchAdapter(manager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(manager, lifecycle) {
+    companion object {
         val matchListTab = listOf(
-            Tab(UpcomingMatchesFragment(), "Upcoming"),
-            Tab(RecentMatchesFragment(), "Recent"),
-            Tab(TTwentyMatchesFragment(), "T20"),
-            Tab(ODIMatchesFragment(), "ODI"),
-            Tab(TestMatchesFragment(), "TEST"),
+            Tab(MatchesFragment(), "UPCOMING"),
+            Tab(MatchesFragment(), "RECENT"),
+            Tab(MatchesFragment(), "T20"),
+            Tab(MatchesFragment(), "ODI"),
+            Tab(MatchesFragment(), "TEST"),
             Tab(MatchesFragment(), "ALL")
         )
     }
+
+    private fun addBundle(
+        fragment: Fragment,
+        keyCategory: String,
+        categoryValue: String
+    ): Fragment {
+        val bundle = Bundle()
+        bundle.putString(keyCategory, categoryValue)
+        fragment.arguments = bundle
+        return fragment
+    }
+
     override fun getItemCount(): Int {
-        return  matchListTab.size
+        return matchListTab.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        return matchListTab[position].fragment
+        return when (position) {
+            0 -> addBundle(
+                matchListTab[position].fragment,
+                "category",
+                matchListTab[position].category
+            )
+            1 -> addBundle(
+                matchListTab[position].fragment,
+                "category",
+                matchListTab[position].category
+            )
+            2 -> addBundle(
+                matchListTab[position].fragment,
+                "category",
+                matchListTab[position].category
+            )
+            3 -> addBundle(
+                matchListTab[position].fragment,
+                "category",
+                matchListTab[position].category
+            )
+            4 -> addBundle(
+                matchListTab[position].fragment,
+                "category",
+                matchListTab[position].category
+            )
+            5 -> addBundle(
+                matchListTab[position].fragment,
+                "category",
+                matchListTab[position].category
+            )
+            else -> addBundle(
+                matchListTab[position].fragment,
+                "category",
+                matchListTab[position].category
+            )
+        }
     }
 }
